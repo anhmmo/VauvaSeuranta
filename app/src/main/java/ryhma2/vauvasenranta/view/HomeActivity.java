88e1621196449bf82257b22d94e9361dacf149e1
anhmmo <aun@metropolia.fi>
2019-12-09 10:10:48 +0200

    update sovellus

--- app/src/main/java/ryhma2/vauvasenranta/view/HomeActivity.java
+++ app/src/main/java/ryhma2/vauvasenranta/view/HomeActivity.java
@@ -59,7 +59,7 @@ public class HomeActivity extends AppCompatActivity {
                 meista.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        Intent intent = new Intent(HomeActivity.this, MaitoActivity.class);
+                        Intent intent = new Intent(HomeActivity.this, MeistaTiedot.class);
                         startActivity(intent);
                     }
                 });
