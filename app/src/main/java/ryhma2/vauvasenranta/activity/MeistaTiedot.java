202802b298b8bfdb1ecc17433c7d9d4a8a142697
anhmmo <aun@metropolia.fi>
2019-12-09 10:09:50 +0200

    update sovellus

---
+++ app/src/main/java/ryhma2/vauvasenranta/activity/MeistaTiedot.java
@@ -0,0 +1,19 @@
+package ryhma2.vauvasenranta.activity;
+
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+
+import ryhma2.vauvasenranta.R;
+
+public class MeistaTiedot extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meista_tiedot);
+
+
+    }
+
+}
