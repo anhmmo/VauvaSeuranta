ec5213869df449c7dec34041e7ad2315635913a6
anhmmo <aun@metropolia.fi>
2019-12-09 10:53:59 +0200

    menu lisätty

---
+++ app/src/main/java/ryhma2/vauvasenranta/activity/MeistaTiedot.java
@@ -0,0 +1,20 @@
+package ryhma2.vauvasenranta.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import ryhma2.vauvasenranta.R;
+
+
+public class MeistaTiedot extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meista_tiedot);
+    }
+
+}
