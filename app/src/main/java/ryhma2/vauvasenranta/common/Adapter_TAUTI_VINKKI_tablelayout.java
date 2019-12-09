8f478e81634ad97a1e9ef423da211965755338cc
anhmmo <aun@metropolia.fi>
2019-12-09 19:57:26 +0200

    javadoc comment

--- app/src/main/java/ryhma2/vauvasenranta/common/Adapter_TAUTI_VINKKI_tablelayout.java
+++ app/src/main/java/ryhma2/vauvasenranta/common/Adapter_TAUTI_VINKKI_tablelayout.java
@@ -6,16 +6,20 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 
 import ryhma2.vauvasenranta.fragment.VinkkiFragment;
 import ryhma2.vauvasenranta.fragment.TautiJaLaakeFragment;
-
+/**
+ * <h1>tablelayout</h1>
+ * Play, stop, resume and loop sounds
+ *calling tab by choosing position of item
+ * sample https://stackoverflow.com/questions/26822516/how-to-display-3-fragments-inside-pageviewer
+ * sample from https://github.com/nahuel-cci/1erParcial/blob/873dfb3413e67058da66b916a56fc9f1eb69dd02/1erParcial/app/src/main/java/com/example/nahuel/a1erparcial/SampleFragmentPagerAdapter.java
+ *
+ */
 
 public class Adapter_TAUTI_VINKKI_tablelayout extends FragmentStatePagerAdapter {
     public Adapter_TAUTI_VINKKI_tablelayout(FragmentManager fm) {
         super(fm);
     }
 
-    //calling tab by choosing position of item
-    //sample https://stackoverflow.com/questions/26822516/how-to-display-3-fragments-inside-pageviewer
-    // sample from https://github.com/nahuel-cci/1erParcial/blob/873dfb3413e67058da66b916a56fc9f1eb69dd02/1erParcial/app/src/main/java/com/example/nahuel/a1erparcial/SampleFragmentPagerAdapter.java
 
 
 
